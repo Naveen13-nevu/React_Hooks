1e780c636e73c2103ce6f78ba7bad41c0523ffc9
Naveen13-nevu <naveenkumar12634@gmail.com>
2026-05-14 17:49:24 +0530

    update

---
+++ ReactForm-practice/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+
+const App = () => {
+  return (
+    <>
+    
+    <h1>Register form</h1>
+
+    <div className="container">
+      
+    </div>
+        
+    </>
+  )
+}
+
+export default App
+
+
+
+
